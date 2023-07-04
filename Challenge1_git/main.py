4bf98f0cccec11b030833e98c504569d6f2743bf
Kiongo-Bob <njoguub@gmail.com>
2023-07-04 16:06:42 +0300

    Python branch update

--- Challenge1_git/main.py
+++ Challenge1_git/main.py
@@ -1,2 +1,5 @@
 name = str(input("Enter your name: "))
 print(f"{name}, don't quit. You've got this!")
+
+name = str(input("For branch name type [Yes] esle [No] "))
+print(f"You're working in the {name} branch.")
