a74d0157afc1efc4f01f893ae6dc76443a9f479d
Kiongo-Bob <njoguub@gmail.com>
2023-07-04 12:46:10 +0300

    Simple content

---
+++ Challenge1_git/main.py
@@ -0,0 +1,2 @@
+name = str(input("Enter your name: "))
+print(f"{name}, don't quit. You've got this!")
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository comprises the projects undertaken in the QA/QE training at The Jitu.
